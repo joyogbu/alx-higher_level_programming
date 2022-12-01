bd9549aa593c759c5cfb86b80c15429535fbbd67
joyogbu <obyjoy@yahoo.com>
2022-12-01 15:09:51 -0800

    task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
+import import_add
 from add_0 import add
-
-
-def main():
-    pass
-
 
 a = 1
 b = 2
 print("{} + {} = {}" .format(1, 2, add(1, 2)))
 if __name__ == "__main__":
-    main()
+    import_add.main()
