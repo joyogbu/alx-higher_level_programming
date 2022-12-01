c6b6743088ea7344c2d9a1583a3efa5d7c44f8c1
joyogbu <obyjoy@yahoo.com>
2022-12-01 15:22:13 -0800

    corrected0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-import import_add
 from add_0 import add
+
+def main():
+    pass
+
 
 a = 1
 b = 2
 print("{} + {} = {}" .format(1, 2, add(1, 2)))
 if __name__ == "__main__":
-    import_add.main()
+    main()
