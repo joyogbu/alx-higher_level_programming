1e6f6cbab76486c17ee4be25a6a85682ef6e8f8a
joyogbu <obyjoy@yahoo.com>
2022-12-02 05:34:43 -0800

    task 2 correctiin

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    l = len(sys.argv[1:])
+    length = len(sys.argv[1:])
     ag = "argument"
     index = 1
-    if int(l) == 0:
+    if int(length) == 0:
         ag = ag + 's' + '.'
-    if int(l) > 1:
+    if int(length) > 1:
         ag = ag + "s" + ":"
-    print("{:d} {}".format(l, ag))
+    print("{:d} {}".format(length, ag))
     for arg in range(1, len(sys.argv)):
         if sys.argv[index] == sys.argv[0]:
             continue
