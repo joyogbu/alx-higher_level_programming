4bdba1d2809b71afe76dce030967adfcf537c37a
joyogbu <obyjoy@yahoo.com>
2022-12-02 15:48:38 -0800

    task4 correction

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
+#import hidden_4
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    list_names = names.sort
-    for i in list_names:
-        if names[0:2] != '__':
-             print(list_names)
+    names = dir()
+    #list_names = names.sort
+    for i in names:
+        if i[0:2] != '__':
+            print(i)
