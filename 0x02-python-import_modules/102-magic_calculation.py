9376b0e1a924b6ecfa0fcf9d87128f58e80c5b2c
joyogbu <obyjoy@yahoo.com>
2022-12-03 16:34:28 -0800

    task no 8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import dis
 def magic_calculation(a, b):
-    from calculator_1 import add, sub
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
