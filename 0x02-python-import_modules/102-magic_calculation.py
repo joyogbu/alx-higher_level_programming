35478c335a314af6f985e4067fc6d68d3b2c5735
joyogbu <obyjoy@yahoo.com>
2022-12-03 16:37:26 -0800

    corrected task 8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import dis
+
+
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
@@ -9,6 +11,3 @@ def magic_calculation(a, b):
         return (c)
     else:
         return sub(a, b)
-# bytecode = dis.dis(magic_calculation)
-# for instr in bytecode:
-    #print(instr.opname)
