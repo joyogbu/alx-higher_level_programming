5efc8e5fc139e38463821385d66d0b968878fd90
joyogbu <obyjoy@yahoo.com>
2022-12-07 02:41:47 -0800

    task no7 correction

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    #my_tuple = [(x[0] + y[0], x[1] + y[1]) for x in tuple_a for y in tuple_b]
-    #new_t = (0, 0)
     if len(tuple_b) < 2:
         tuple_b = tuple_b + (0, 0)
     if len(tuple_a) < 2:
