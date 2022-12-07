72f824b17a619bb113be11a7f274d055f5782a59
joyogbu <obyjoy@yahoo.com>
2022-12-07 14:55:30 -0800

    test10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
         return None
     newl = []
     for i in my_list:
-        if my_list[i] % 2 == 0:
+        if i % 2 == 0:
             newl.append(True)
         else:
             newl.append(False)
