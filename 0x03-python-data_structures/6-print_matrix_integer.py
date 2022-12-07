a383e9f5c12b741ec3c2b189e3aa9aba30ea64bc
joyogbu <obyjoy@yahoo.com>
2022-12-07 07:44:20 -0800

    main6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    #for i in range(len(matrix)):
-        #for j in range(len(matrix[i])):
-            #print("{:d}".format(matrix[i][j]), end = "")
-            #if j != len(matrix[i]) - 1:
-                #print(" ", end = " ")
-    #x = '\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in matrix])
-    #print(x)
     for row in matrix:
        for val in row:
-           print("{:d}".format(val), end = "")
            if row.index(val) != (len(matrix) - 1):
-               print(" ", end = " ")
-               #print("{:d}".format(val), end=" ")
-           #else:
-               #print("{:d}".format(val), end = "")
-       #if val != len(matrix) - 1:
-       #print()
+               print("{:d}".format(val), end=" ")
+           else:
+               print("{:d}".format(val), end = "")
+       print()
