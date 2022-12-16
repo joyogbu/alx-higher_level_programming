b67c2bb80704b57df2558a90fb7d97fd32989d56
joyogbu <obyjoy@yahoo.com>
2022-12-16 09:09:57 -0800

    task 2 edit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     cop = my_list.copy()
+    if search not in my_list:
+        return
     idx = 0
     for idx in cop:
         if idx == search:
