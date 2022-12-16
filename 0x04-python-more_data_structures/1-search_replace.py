5fdae46a2d62ad714095cca71cdf0de23c328d61
joyogbu <obyjoy@yahoo.com>
2022-12-16 10:38:14 -0800

    corection

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,7 +4,7 @@ def search_replace(my_list, search, replace):
     if search not in my_list:
         pass
     if my_list == []:
-        pass
+        return
     arr = []
     idx = 0
     for idx in range(len(cop)):
