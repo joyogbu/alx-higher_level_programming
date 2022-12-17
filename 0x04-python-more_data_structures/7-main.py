77082028295cf867c77d6e3ef1e5bd40c5346ffd
joyogbu <obyjoy@yahoo.com>
2022-12-17 03:29:47 -0800

    task no 7edited

--- 0x04-python-more_data_structures/7-main.py
+++ 0x04-python-more_data_structures/7-main.py
@@ -2,8 +2,9 @@
 update_dictionary = __import__('7-update_dictionary').update_dictionary
 print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
+
 a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-new_dict = update_dictionary(a_dictionary, 'language', "Python")
+new_dict = update_dictionary(a_dictionary, 'e', 89)
 print_sorted_dictionary(new_dict)
 print("--")
 print_sorted_dictionary(a_dictionary)
