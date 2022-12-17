ff45b25831349ec100da4db10aae312905f201e4
joyogbu <obyjoy@yahoo.com>
2022-12-17 04:10:22 -0800

    edited 8

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    mdic ={ky: val * 2 for ky, val in a_dictionary.items()}
+    mdic = {ky: val * 2 for ky, val in a_dictionary.items()}
     return(mdic)
