402519cf4ddd967c50a50f68b1d8f588cdd2bd2f
joyogbu <obyjoy@yahoo.com>
2022-12-17 03:50:15 -0800

    task 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    del a-dictionary[key]
-    return(a_dictionary)
+    if key not in a_dictionary:
+        return(a_dictionary)
+    else:
+        del a_dictionary[key]
+        return(a_dictionary)
