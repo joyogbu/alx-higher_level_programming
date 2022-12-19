b6e3e7ff4de1149689334d911f0cdb071c68a0d0
joyogbu <obyjoy@yahoo.com>
2022-12-19 14:25:49 -0800

    corrected task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,6 +4,5 @@ def safe_print_integer(value):
         print("{:d}".format(value))
     except (ValueError, IndexError, TypeError) as e:
         pass
-        #if isinstance(e, Exception):
         return(False)
     return(True)
