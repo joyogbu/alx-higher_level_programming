9483810968e3bb02750a864803748f8663d8448a
joyogbu <obyjoy@yahoo.com>
2022-12-20 11:19:11 -0800

    task no 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """class that defines a square
+    """
+    def __init__(self, size=None):
+        """instatiating with private instance attribute
+        Args:
+        size (int): private instance attribute
+        """
+        self.__size = size
