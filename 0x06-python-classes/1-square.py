67c664bda9bf33da0ee193908324c9370b755d8e
joyogbu <obyjoy@yahoo.com>
2022-12-20 11:27:47 -0800

    edited1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""defining an empty square and instatiating with a size attribute """
+
+
 class Square:
-    """class that defines a square
-    """
+    """class that defines a square"""
     def __init__(self, size=None):
         """instatiating with private instance attribute
+
         Args:
         size (int): private instance attribute
         """
