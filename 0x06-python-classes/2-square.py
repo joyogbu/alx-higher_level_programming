f996139c01716cf379d560433e6332e146e5b79d
joyogbu <obyjoy@yahoo.com>
2022-12-20 11:54:15 -0800

    edited 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""defining a empty square class"""
+
+
 class Square:
     """Represents a class Square"""
     def __init__(self, size=0):
@@ -12,4 +15,3 @@ class Square:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
-
