2b57e83518e778ff5a622228f2cd02d8eca1a64e
joyogbu <obyjoy@yahoo.com>
2022-12-20 11:50:11 -0800

    task no 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    """Represents a class Square"""
+    def __init__(self, size=0):
+        """Initializing with data
+
+        Args:
+        size (int): size of the square
+        """
+        self.__size = size
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
