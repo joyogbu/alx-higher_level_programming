ac5c272d5d72788da1ff01b7132d4c99b7e71ec4
joyogbu <obyjoy@yahoo.com>
2023-01-03 08:17:41 -0800

    edited no 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,7 +5,8 @@
 class Rectangle:
     """ define the rectangle class
     """
-    number_of_instances = 0;
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """instatiating an object
         Args:
