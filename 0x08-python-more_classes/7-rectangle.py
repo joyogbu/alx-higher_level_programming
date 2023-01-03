03eb6a8698cb09814ebe03c3f296262856de5812
joyogbu <obyjoy@yahoo.com>
2023-01-03 09:44:54 -0800

    edited no 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -71,7 +71,7 @@ class Rectangle:
             return string
         for i in range(self.__height):
             for j in range(self.__width):
-                string += str(Rectangle.print_symbol)
+                string += str(self.print_symbol)
             if i < self.__height - 1:
                 string += '\n'
         return string
