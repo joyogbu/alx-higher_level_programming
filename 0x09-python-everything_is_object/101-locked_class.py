b75ecb8793a722ff30e2c0c9eb46dd49333e83cd
joyogbu <obyjoy@yahoo.com>
2023-01-04 10:53:29 -0800

    no101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""class prevent dynamic creation of class and instance attribute"""
+
+
 class LockedClass:
+    """representing LockedClass class"""
     __slots__ = ['first_name'] 
