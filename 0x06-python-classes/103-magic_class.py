e9404990be73e45a02102c75bdd4436a756ed8b8
joyogbu <obyjoy@yahoo.com>
2023-01-05 09:37:38 -0800

    task 103

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""defing magic class"""
+
+
+class MagicClass:
+    """representing the magic class"""
+
+    def __init__(self):
+        """initializing themagic class"""
+        self.__radius = radius
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+            self.__radius = None
+
+    def area(self):
+        """defining area"""
+        return math.pi(self.__radius ** 2)
+
+    def circumference(self):
+        """defining circumference"""
+        return 2 * (math.pi(self.__radius))
+
