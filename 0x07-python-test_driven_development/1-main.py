ee3dc84afa74ad8b4ef1c1035fd13f2e8b70a8f5
joyogbu <obyjoy@yahoo.com>
2023-01-06 12:35:08 -0800

    task no 2

--- 0x07-python-test_driven_development/1-main.py
+++ 0x07-python-test_driven_development/1-main.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [
-    [5, 2, 3],
-    [7, 9]
-]
-print(matrix_divided(matrix, 3))
+matrix = [[4, "3"], [8, 3]]
+print(matrix_divided(matrix, 2))
 print(matrix)
