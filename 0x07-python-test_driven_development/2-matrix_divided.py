90eb0f8afc75a622bfcc53eaca9b3e0558ab3482
joyogbu <obyjoy@yahoo.com>
2023-01-06 16:04:31 -0800

    edited task no 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,13 +8,13 @@ def matrix_divided(matrix, div):
 
     for row in matrix:
         if type(row) is not list:
-            raise TypeError('matrix must be a matrix \
-                    (list of lists) of integers/floats')
+            raise TypeError("matrix must be a matrix "
+                            "(list of lists) of integers/floats")
     for row in matrix:
         for item in row:
             if type(item) is not int and type(item) is not float:
-                raise TypeError("matrix must be a matrix \
-                        (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix "
+                                "(list of lists) of integers/floats")
     for row in matrix:
         if len(row) != len(matrix[0]):
             raise TypeError('Each row of the matrix must have the same size')
