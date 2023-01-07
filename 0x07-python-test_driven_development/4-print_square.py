3590c7d5f37117cea65f1e15b784bd00a6fa38ae
joyogbu <obyjoy@yahoo.com>
2023-01-06 20:45:59 -0800

    task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def print_square(size):
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    string = ""
+    for i in range(size):
+        for v in range(size):
+            string += "#"
+        if i < size:
+            string += '\n'
+    print(string)
