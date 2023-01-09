69854b282bd417f7fc6d4627b159e8858763367f
joyogbu <obyjoy@yahoo.com>
2023-01-09 04:21:01 -0800

    edited task1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""this module returns the number of attributes and methods of an object in in a list form
+"""
+
+
 def lookup(obj):
+    """defining the function
+    """
     return([dir(obj)])
