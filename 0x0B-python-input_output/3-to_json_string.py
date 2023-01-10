eddb81c4c92b9d766b993ca5b3538f512d4fe9f5
joyogbu <obyjoy@yahoo.com>
2023-01-10 06:21:25 -0800

    task3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""return the JSON representation of a string object
+"""
+
+
+import json
+def to_json_string(my_obj):
+    """
+    defining the function
+    """
+    return (json.dumps(my_obj))
+
+
