a618b5064d7f22702a28d0591b22f5b1607b949c
joyogbu <obyjoy@yahoo.com>
2023-01-10 05:22:17 -0800

    task no1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""writing to a text file
+"""
+
+
 def write_file(filename="", text=""):
+    """defining the function
+    """
     with open(filename, 'w', encoding="utf-8") as f:
         no_written = f.write(text)
         return(no_written)
