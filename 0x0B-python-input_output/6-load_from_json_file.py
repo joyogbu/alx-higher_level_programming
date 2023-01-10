ca6c526c4cc11009a4a6c14034f50469f93727fe
joyogbu <obyjoy@yahoo.com>
2023-01-10 12:28:06 -0800

    task no 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,4 +9,4 @@ import json
 def load_from_json_file(filename):
     """defining the functionz"""
     with open(filename, 'w', encoding="utf-8") as f:
-        json.loads(f)
+        return json.loads(f)
