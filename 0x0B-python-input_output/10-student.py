4432b67612a411278e96f9f004c69e346931076e
joyogbu <obyjoy@yahoo.com>
2023-01-11 08:34:37 -0800

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""class student"""
+
+
+class Student:
+    """representing the student class"""
+    def __init__(self, first_name, last_name, age):
+        """initializing the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of
+        a student instance"""
+        if type(attrs) is list:
+            for i in attrs:
+                    return {i: v for i, v in self.__dict__.items() if i in attrs}
+        return self.__dict__
