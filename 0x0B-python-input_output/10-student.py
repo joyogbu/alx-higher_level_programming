7e8064e32f1081d66ea8baf377ffce6d0501641c
joyogbu <obyjoy@yahoo.com>
2023-01-11 11:31:00 -0800

    tsk no10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,7 +13,6 @@ class Student:
     def to_json(self, attrs=None):
         """retrieves a dictionary representation of
         a student instance"""
-        if type(attrs) is list:
-            for i in attrs:
-                    return {i: v for i, v in self.__dict__.items() if i in attrs}
+        if type(attrs) is list and all(type(elem) is str for elem in attrs):
+            return {i: v for i, v in self.__dict__.items() if i in attrs}
         return self.__dict__
