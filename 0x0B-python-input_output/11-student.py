3bb3b623aa6de557baba68d323583670754ca8e9
joyogbu <obyjoy@yahoo.com>
2023-01-11 12:30:55 -0800

    task no 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -19,11 +19,5 @@ class Student:
 
     def reload_from_json(self, json):
         """replace all attributes of the Student instance"""
-        #d = {}
         for i, v in json.items():
-            #d[i] = v
-            #d[i] = v
             self.__dict__ = json
-        #print("hd is {}".format(d))
-            #return(d)
-        
