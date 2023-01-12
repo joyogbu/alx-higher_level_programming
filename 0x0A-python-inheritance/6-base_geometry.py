c8741674f766f07599a15fda7ee6312386877ad0
joyogbu <obyjoy@yahoo.com>
2023-01-11 16:12:28 -0800

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """representing the class"""
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
