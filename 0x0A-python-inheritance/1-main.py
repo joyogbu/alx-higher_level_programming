43771570d305c2c029857f82e13f0d998751b35b
joyogbu <obyjoy@yahoo.com>
2023-01-12 15:32:24 -0800

    task 1edited

--- 0x0A-python-inheritance/1-main.py
+++ 0x0A-python-inheritance/1-main.py
@@ -2,11 +2,11 @@
 MyList = __import__('1-my_list').MyList
 
 my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
+#my_list.append(1)
+#my_list.append(4)
+#my_list.append(2)
+#my_list.append(3)
+my_list.append("joy")
 print(my_list)
 my_list.print_sorted()
 print(my_list)
