a16213003513af932df85c9864b4a58178598ecc
joyogbu <obyjoy@yahoo.com>
2023-01-13 06:41:47 -0800

    task8

--- 0x0A-python-inheritance/8-main.py
+++ 0x0A-python-inheritance/8-main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 Rectangle = __import__('8-rectangle').Rectangle
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 r = Rectangle(3, 5)
 
 print(r)
@@ -15,3 +16,5 @@ try:
     r2 = Rectangle(4, True)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
+
+    print(issubclass(Rectangle, BaseGeometry))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,22 +1,5 @@
 #!/usr/bin/python3
-"""empty class BaseGeometry"""
-
-
-class BaseGeometry:
-    """representing the class"""
-    pass
-
-    def area(self):
-        """defining area"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates value"""
-        if type(value) != int:
-            raise TypeError(name + " must be an integer")
-        if value <= 0:
-            raise ValueError(name + " must be greater than 0")
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """class rectangle that inherits from BaseGeometry"""
 
@@ -24,7 +7,7 @@ class Rectangle(BaseGeometry):
         """instantiating the class"""
         self.__width = width
         self.__height = height
+        #super().integer(self)
         #BaseGeometry.__init__(self)
-        BaseGeometry.integer_validator(self, "width", width)
-        BaseGeometry.integer_validator(self, "height", height)
-
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
