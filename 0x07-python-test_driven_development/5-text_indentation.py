f220fd0f9c64bd4db333e692fbb6155b9ec9a222
joyogbu <obyjoy@yahoo.com>
2023-01-14 15:25:11 -0800

    task5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""print two new lines after '.', '?' and ':' in a text
+"""
+
+
+def text_indentation(text):
+    """defining the function"""
+    text2 = text.replace('.', '.\n\n').replace('?', '?\n\n').replace(':', ':\n\n')
+    for i in range(len(text)):
+        text2 = text2.replace('\n', '\n')
+    print(text2)
