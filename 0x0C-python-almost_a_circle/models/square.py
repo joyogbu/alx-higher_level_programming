05f31f28f18feec112960fe69c053eb6b2fa8a96
joyogbu <obyjoy@yahoo.com>
2023-01-19 01:20:14 -0800

    task[D11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,18 +4,28 @@
 from models.base import Base
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """representing the square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
         """instantiating the class"""
-        
+
         super().__init__(size, size, x, y, id)
         self.size = size
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """string representation of the rectangle object"""
         string = "[" + self.__class__.__name__ + "]" + " (" + \
-                str(self.id) + ") " + str(self.x) + "/" + \
-                str(self.y) + " - " + str(self.width)
+            str(self.id) + ") " + str(self.x) + "/" + \
+            str(self.y) + " - " + str(self.width)
         return (string)
