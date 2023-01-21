e299586398b0755614321692470811e7eb30f09e
joyogbu <obyjoy@yahoo.com>
2023-01-21 04:52:26 -0800

    task edited4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,9 +5,11 @@
 
 def text_indentation(text):
     """defining the function"""
-    if type(text) != str:
-        raise TypeError('test must be a string')
-    text2 = text.replace('.', '.\n\n').replace('?', '?\n\n').replace(':', ':\n\n')
-    for i in range(len(text)):
-        text2 = text2.replace('\n ', '\n')
-    print(text2, end='')
+    if not isinstance(text, str):
+    #if type(text) is not str:
+        raise TypeError("test must be a string")
+    else:
+        text2 = text.replace('.', '.\n\n').replace('?', '?\n\n').replace(':', ':\n\n')
+        for i in range(len(text)):
+            text2 = text2.replace('\n ', '\n')
+        print(text2, end='')
