57eb71a7325e0b8e8eac9405165154488d67e85f
joyogbu <obyjoy@yahoo.com>
2023-02-15 09:29:03 -0800

    edited task4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- create a table with not null id
-CREATE TABLE IF NOT EXISTS id_not_null(id INT PRIMARY KEY DEFAULT 1, name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS id_not_null(id INT NOT NULL DEFAULT 1, name VARCHAR(256))
