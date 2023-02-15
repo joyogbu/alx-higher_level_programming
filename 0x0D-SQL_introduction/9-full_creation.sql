ebcbcaad2e77bf0be0baf97bc5e9ac1c635453e0
joyogbu <obyjoy@yahoo.com>
2023-02-14 21:07:32 -0800

    task9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- create second table
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT)
+INSERT INTO TABLE second_table((1, 'Johm', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George, 8))
