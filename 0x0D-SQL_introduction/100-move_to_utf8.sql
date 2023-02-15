0dcbbcf6b383231f1a0e767f75c88ffff63dd5cf
joyogbu <obyjoy@yahoo.com>
2023-02-15 03:23:37 -0800

    testing task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,7 @@
 -- convert database to utf8mb4
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci
+USE hbtn_0c_0
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 -- for table
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE = utf8mb4_unicode_ci
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE = utf8mb4_unicode_ci;
 -- for column
-ALTER TABLE first_table CHANGE COLUMN name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+ALTER TABLE first_table CHANGE COLUMN name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
