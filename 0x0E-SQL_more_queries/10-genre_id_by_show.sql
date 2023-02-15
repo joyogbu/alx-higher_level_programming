10c6a36efedf34de0303a73cf6ed0b93ba0c2f5d
joyogbu <obyjoy@yahoo.com>
2023-02-15 14:10:34 -0800

    edited no 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- list all records contained in a table from a mysql dump
-mysql < hbtn_0d_tvshows.sql
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id
+-- mysql < hbtn_0d_tvshows.sql
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
