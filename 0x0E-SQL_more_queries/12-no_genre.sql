9bdfc16d8e5bcf1e7ad471bf47e825ae0bb5189b
joyogbu <obyjoy@yahoo.com>
2023-02-16 00:24:28 -0800

    correction on 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,3 +1,3 @@
 -- lisall records contained in a database
 -- mysql < hbtn_0d_tvshows.sql
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows RIGHT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres RIGHT JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
