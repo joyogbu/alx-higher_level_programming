b4b96b4cfc28343db088bfabf9ddb3f7e394a43e
joyogbu <obyjoy@yahoo.com>
2023-02-15 18:26:37 -0800

    task 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,3 @@
+-- lisall records contained in a database
+-- mysql < hbtn_0d_tvshows.sql
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows RIGHT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
