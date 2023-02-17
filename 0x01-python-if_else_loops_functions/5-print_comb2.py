7b3934e297cfbe0476ae326ef9dc3d93ba2996e2
joyogbu <obyjoy@yahoo.com>
2023-02-17 00:10:24 -0800

    task5 edited

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if i == 9 and j == 9:
-            print("{}{}".format(i, j))
+for i in range(0, 100):
+        if i == 99:
+            print("{}".format(i))
         else:
-            print("{}{}".format(i, j), end=", ")
+            print("{:02d}".format(i), end=", ")
