54dd027e5c7a5cc04b527e519ab4f426c2ca797c
joyogbu <obyjoy@yahoo.com>
2023-03-13 18:00:26 +0000

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 const arg = process.argv;
 console.log(typeof Number(arg[2]));
 const arg1 = Number(arg[2]);
-if (Number.isInteger(arg1)){
-  console.log('My number is ' + arg1);
+if (Number.isInteger(arg1)) {
+  console.log('My number:' + arg1);
 } else {
   console.log('Not a number');
 }
