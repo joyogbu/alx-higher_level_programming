718e3d8e0edab94811b92de091ef568c9c4d2c23
joyogbu <obyjoy@yahoo.com>
2023-03-13 15:14:06 +0000

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,10 +3,8 @@ const arg = process.argv;
 const len = arg.length;
 if (len === 2) {
   console.log('No argument');
-}
-else if (len === 3) {
+} else if (len === 3) {
   console.log('Argument found');
-}
-else {
+} else {
   console.log('Arguments found');
 }
