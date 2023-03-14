1270d245f3703ac37c78c2106eea5385771cc056
joyogbu <obyjoy@yahoo.com>
2023-03-14 09:31:03 +0000

    task 10 edited

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const arg = process.argv;
-arg1 = Number(arg[2]);
-function factorial(n) {
+const arg1 = Number(arg[2]);
+function factorial (n) {
   if (isNaN(arg1)) {
     return 1;
   }
-  if (n == 0 || n == 1) {
+  if (n === 0 || n === 1) {
     return 1;
   } else {
-      return n * factorial(n - 1);
-    }
+    return n * factorial(n - 1);
+  }
 }
 console.log(factorial(arg1));
