e8e548fd111b4dc3e7b0abf02616d4242904316f
joyogbu <obyjoy@yahoo.com>
2023-03-14 00:06:07 +0000

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const arg =  process.argv;
+let myStr = '';
+let i;
+let j;
+const arg1 = Number(arg[2]);
+if (Number.isInteger(arg1)) {
+  for (i = 0; i < arg1; i++) {
+    for (j = 0; j < arg1; j++) {
+      myStr += 'X';
+    }
+    if (i < arg1) {
+      //console.log('\n');
+      myStr += '\n';
+    }
+ }
+  console.log(myStr);
+//}
+} else {
+  console.log('Missing size');
+}
