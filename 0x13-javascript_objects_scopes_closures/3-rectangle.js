c0b49cde37e8ef052cf54ddc149b72899381b778
joyogbu <obyjoy@yahoo.com>
2023-03-14 20:37:11 +0000

    task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,9 +1,16 @@
 #!/usr/bin/node
+let i;
 class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+    }
+  }
+
+  print () {
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 }
