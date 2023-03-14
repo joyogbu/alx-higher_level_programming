bef7ba311d97541676454e9e459405305395fa35
joyogbu <obyjoy@yahoo.com>
2023-03-14 14:49:19 +0000

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const arg = process.argv.slice(2);
+let x;
+if (arg.length === 0 || arg.length === 1) {
+  x = 0;
+} else {
+  x = Math.max.apply(null, arg);
+  arg.splice(arg.indexOf(x), 1);
+  x = Number(Math.max.apply(null, arg));
+}
+console.log(x);
