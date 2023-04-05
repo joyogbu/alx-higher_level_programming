2abb344136aacd16033e05df93bfe897150345a8
joyogbu <obyjoy@yahoo.com>
2023-04-05 10:14:22 -0700

    task 5

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ find a peak in an unsorted list of integers """
+
 def find_peak(list_of_integers):
+    """ define yhe function """
     if len(list_of_integers) > 0:
         list_of_integers.sort()
         return list_of_integers[-1]
