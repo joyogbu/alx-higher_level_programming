0547ae4874bc6fadfe15cc004cb05dd3cb3310a8
Joy Ogbu <obyjoy@yahoo.com>
2023-04-10 14:00:01 -0700

    edited task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""post an email"""
+
+
 import urllib.request
 import sys
+
+
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = {'Your email is' : email}
-    #data = urllib.parse.urlencode(value)
-    #data = data.encode()
+    # data = urllib.parse.urlencode(value)
+    # data = data.encode()
     req = urllib.request.Request(url, data) 
     with urllib.request.urlopen(req) as response:
         out = response.headers
