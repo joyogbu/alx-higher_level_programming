10281a58b706780d3f4fb6f442a3470c25e9bc57
Joy Ogbu <obyjoy@yahoo.com>
2023-04-10 08:29:09 -0700

    edited task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as \
             response:
         output = response.read()
+        print("Body response")
         print('\t- type: {}'.format(type(output)))
     print('\t- content: {}'.format(output))
     print('\t- utf8 content: {}'.format(output.decode("utf-8")))
