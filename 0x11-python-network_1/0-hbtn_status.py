fcb4b861a486a40d82201d8909560f2b4fab34bf
Joy Ogbu <obyjoy@yahoo.com>
2023-04-10 08:16:53 -0700

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""using utllib to frtch a url"""
+
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as \
+            response:
+        output = response.read()
+        print('\t- type: {}'.format(type(output)))
+    print('\t- content: {}'.format(output))
+    print('\t- utf8 content: {}'.format(output.decode("utf-8")))
