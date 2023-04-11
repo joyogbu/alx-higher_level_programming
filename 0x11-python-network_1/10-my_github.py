88c88d86563018f597db53957649ad31731c3402
joyogbu <obyjoy@yahoo.com>
2023-04-11 14:58:49 -0700

    task 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""send github credentials and print id"""
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    req = requests.get('https://api.github.com/user', auth=(username, password))
+    print(req.json()['id'])
