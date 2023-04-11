f5f08687392c80046a30240f4934ae0a71b104b8
joyogbu <obyjoy@yahoo.com>
2023-04-11 01:43:57 -0700

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""srnd a URL request and display the X-Request-Id data"""
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    out = req.headers['X-Request-Id']
+    print(out)
