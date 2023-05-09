acb5e30c66bfa4bf8444f677957c3563b432fa7d
joyogbu <obyjoy@yahoo.com>
2023-05-09 10:06:12 +0000

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
 const arg = process.argv;
-const url = arg[1];
-console.log(url);
+const url = arg[2];
+const fs = require("fs");
+fs.readFile(url, "utf-8", function(err, buf) {
+         if(err) {
+              console.log(err);
+         }
+         console.log(buf);
+});
