54d5f9b51cf48f1411b87252c09404eee81ee00d
joyogbu <obyjoy@yahoo.com>
2023-06-21 10:36:21 -0700

    task 4

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Safe from SQL injections.
-# Usage: ./3-my_safe_filter_states.py <mysql username> \
-#                                     <mysql password> \
-#                                     <database name> \
-#                                     <state name searched>
+'''Displays all values in the states table of the database hbtn_0e_0_usa'''
+# whose name matches that supplied as argument. 
+
+
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
+    '''define the module'''
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
