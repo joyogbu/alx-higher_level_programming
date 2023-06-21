d80093e42c76cfc44c614d0dd21a312a92ce22d1
joyogbu <obyjoy@yahoo.com>
2023-06-21 12:32:59 -0700

    task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
+'''Write a script that lists all State objects from hbtn_0e_6_usa'''
 
+
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+args = sys.argv
+
+if __name__ == "__main__":
+    '''connect and query the database'''
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(args[1], args[2], args[3]))
+    DBSession = sessionmaker(bind=engine)
+    db_session = DBSession()
+    all_states = db_session.query(State).first()
+    print("{}: {}".format(all_states.id, all_states.name))
